a38a9173a4be08ada84f1c363eea6327975e8c9a
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-04-09 22:23:22 +0100

    baseline and background for gas B

--- final_tma_plots.py
+++ final_tma_plots.py
@@ -102,4 +102,57 @@ ax.add_patch(rectA_1)
 plt.gca().invert_xaxis()
 plt.show()
 
-# Plot methane scans
+# Do necessary subtractions for gas B:
+
+dataB1 = pd.read_csv('gasB_20mbar.csv', header=4)
+dataB2 = pd.read_csv('gasB_20_mbar.csv', header=4)
+dataB3 = pd.read_csv('gasB_50mbar.csv', header=4)
+dataB4 = pd.read_csv('gasB_90mbar.csv', header=4)
+dataB5 = pd.read_csv('gasB_120mbar.csv', header=4)
+dataB6 = pd.read_csv('gasB_200mbar.csv', header=4)
+dataB7 = pd.read_csv('gasB_280mbar.csv', header=4)
+
+backgroundB = pd.read_csv('background_gasB.csv', header=4)
+baselineB1 = LinearBaseline(dataB1['wavenumber / cm-1'], dataB1['Absorbance'], 20)
+baselineB2 = LinearBaseline(dataB2['wavenumber / cm-1'], dataB2['Absorbance'], 20)
+baselineB3 = LinearBaseline(dataB3['wavenumber / cm-1'], dataB3['Absorbance'], 20)
+baselineB4 = LinearBaseline(dataB4['wavenumber / cm-1'], dataB4['Absorbance'], 20)
+baselineB5 = LinearBaseline(dataB5['wavenumber / cm-1'], dataB5['Absorbance'], 20)
+baselineB6 = LinearBaseline(dataB6['wavenumber / cm-1'], dataB5['Absorbance'], 20)
+baselineB7 = LinearBaseline(dataB7['wavenumber / cm-1'], dataB7['Absorbance'], 20)
+
+print(max(dataB6['Absorbance']))
+data1_absorbanceB = dataB1['Absorbance'] - backgroundB['Absorbance']
+data1_absorbance_corrB = data1_absorbanceB -baselineB1
+list1 = data1_absorbance_corrB.tolist()
+print(max(list1))
+
+data2_absorbanceB = dataB2['Absorbance'] - backgroundB['Absorbance']
+data2_absorbance_corrB = data2_absorbanceB - baselineB2
+list2 = data2_absorbance_corrB.tolist()
+print(max(list2))
+
+data3_absorbanceB = dataB3['Absorbance'] - backgroundB['Absorbance']
+data3_absorbance_corrB = data3_absorbanceB - baselineB3
+list3 = data3_absorbance_corrB.tolist()
+print(max(list3))
+
+data4_absorbanceB = dataB4['Absorbance'] - backgroundB['Absorbance']
+data4_absorbance_corrB = data4_absorbanceB - baselineB4
+list4 = data4_absorbance_corrB.tolist()
+print(max(list4))
+
+data5_absorbanceB = dataB5['Absorbance'] - backgroundB['Absorbance']
+data5_absorbance_corrB = data5_absorbanceB - baselineB5
+list5 = data5_absorbance_corrB.tolist()
+print(max(list5))
+
+data6_absorbanceB = dataB6['Absorbance'] - backgroundB['Absorbance']
+data6_absorbance_corrB = data6_absorbanceB - baselineB6
+list6 = data6_absorbance_corrB.tolist()
+print(max(list6))
+
+data7_absorbanceB = dataB7['Absorbance'] - backgroundB['Absorbance']
+data7_absorbance_corrB = data7_absorbanceB - baselineB7
+list7 = data7_absorbance_corrB.tolist()
+print(max(list7))
