7d35722776d201aef9379bf18931f98eabe593be
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-04-09 22:24:30 +0100

    plot for gas B

--- final_tma_plots.py
+++ final_tma_plots.py
@@ -20,7 +20,7 @@ def LinearBaseline(x, y, points):
 # Could of course use glob and loops, but...
   
 # Read data
-data1 = pd.read_csv('gasA_10mbar.csv', header=4)
+data1 = pd.read_csv('gasA_10mbar.csv', header=4) #-> this may bring up error in Jupyter
 data2 = pd.read_csv('gasA_20mbar.csv', header=4)
 data3 = pd.read_csv('gasA_50mbar.csv', header=4)
 data4 = pd.read_csv('gasA_100mbar.csv', header=4)
@@ -156,3 +156,85 @@ data7_absorbanceB = dataB7['Absorbance'] - backgroundB['Absorbance']
 data7_absorbance_corrB = data7_absorbanceB - baselineB7
 list7 = data7_absorbance_corrB.tolist()
 print(max(list7))
+
+# Plot scans for gas B:
+
+import matplotlib.patches as patches
+
+plt.rcParams['figure.figsize'] = [18,14]
+plt.rcParams['font.size'] = 14
+plt.rcParams['legend.loc'] = 'upper left'
+plt.xlabel('Wavenumber / $cm^{-1}$')
+plt.ylabel('Absorbance (A)')
+plt.plot(dataB1['wavenumber / cm-1'], data1_absorbance_corrB+0.15, label="10 mbar (max A: 0.035)")
+plt.plot(dataB2['wavenumber / cm-1'], data2_absorbance_corrB+0.2, label="20 mbar (max A: 0.057)")
+plt.plot(dataB3['wavenumber / cm-1'], data3_absorbance_corrB+0.25, label="50 mbar (max A: 0.11)")
+plt.plot(dataB4['wavenumber / cm-1'], data4_absorbance_corrB+0.3, label="100 mbar (max A: 0.19)")
+plt.plot(dataB5['wavenumber / cm-1'], data5_absorbance_corrB+0.39, label="120 mbar (max A: 0.25)")
+plt.plot(dataB7['wavenumber / cm-1'], data7_absorbance_corrB, label="280 mbar (max A: 0.56)")
+plt.legend()
+ax = plt.gca()
+plt.annotate("$CH_{4}$ Vib. 1 (3019 $cm^{-1}$)",
+            xy=(3000, 0.58), xycoords='data',
+            xytext=(2950, 0.65), textcoords='data',
+            arrowprops=dict(arrowstyle="->",
+                            connectionstyle="arc3"),
+            )
+
+plt.annotate("$CH_{4}$ Vib. 2 (1306 $cm^{-1}$)",
+            xy=(1300, 0.54), xycoords='data',
+            xytext=(1500, 0.63), textcoords='data',
+            arrowprops=dict(arrowstyle="->",
+                            connectionstyle="arc3"),
+            )
+
+plt.annotate("$CH_{4}$ Vib. 3 (~2840-2680 $cm^{-1}$)",
+            xy=(2750, 0.402), xycoords='data',
+            xytext=(2900, 0.45), textcoords='data',
+            arrowprops=dict(arrowstyle="->",
+                            connectionstyle="arc3"),
+            )
+plt.annotate("$CH_{4}$ Vib. 4 (~1600-1400 $cm^{-1}$)",
+            xy=(1500, 0.402), xycoords='data',
+            xytext=(2200, 0.42), textcoords='data',
+            arrowprops=dict(arrowstyle="->",
+                            connectionstyle="arc3"), 
+            )
+
+plt.annotate("$CH_{4}$ Base Reading (~3200-3170 $cm^{-1}$)",
+            xy=(3185, 0.402), xycoords='data',
+            xytext=(3850, 0.52), textcoords='data',
+            arrowprops=dict(arrowstyle="->",
+                            connectionstyle="arc3"), 
+            )
+
+plt.annotate("$CH_{4}$ Base Reading (~1240-1180 $cm^{-1}$)",
+            xy=(1200, 0.402), xycoords='data',
+            xytext=(2190, 0.5), textcoords='data',
+            arrowprops=dict(arrowstyle="->",
+                            connectionstyle="arc3"), 
+            )
+plt.annotate("Potential trace gas region ($CH_{3}OH, C_{2}H_{6}$)",
+            xy=(2950, 0.39), xycoords='data',
+            xytext=(2800, 0.33), textcoords='data',
+            arrowprops=dict(arrowstyle="->",
+                            connectionstyle="arc3"), 
+            )
+
+
+
+rect = patches.Rectangle(xy=(2680,0.383), width=160, height=0.02, zorder=3,linewidth=1.5, linestyle='--',edgecolor='r', facecolor='none')
+rect1 = patches.Rectangle(xy=(1400,0.383), width=160, height=0.02, zorder=3,linewidth=1.5, linestyle='--', edgecolor='r', facecolor='none')
+rect2 = patches.Rectangle(xy=(3170,0.383), width=30, height=0.02, zorder=3,linewidth=1.5,linestyle='--', edgecolor='r', facecolor='none')
+rect3 = patches.Rectangle(xy=(1180,0.383), width=60, height=0.02, zorder=3,linewidth=1.5,linestyle='--', edgecolor='r', facecolor='none')
+rect4 = patches.Rectangle(xy=(2850,0.383), width=250, height=0.02, zorder=3,linewidth=1.5,linestyle='--', edgecolor='b', facecolor='none')
+ax.add_patch(rect)
+ax.add_patch(rect1)
+ax.add_patch(rect2)
+ax.add_patch(rect3)
+ax.add_patch(rect4)
+
+plt.gca().invert_xaxis()
+plt.savefig('gasB_mixture.png')
+plt.show()
+
