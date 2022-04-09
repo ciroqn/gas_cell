cc34cfe14f8c459a2ef1ec5117e54a36f578a072
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-04-09 22:22:36 +0100

    plot co2 scans

--- final_tma_plots.py
+++ final_tma_plots.py
@@ -59,3 +59,47 @@ data5_absorbance = data5['Absorbance'] - background['Absorbance']
 data5_absorbance_corr = data5_absorbance - baseline5
 print(max(data5['Absorbance']))
 
+# Plot CO2 scans:
+
+plt.rcParams['figure.figsize'] = [18,14]
+plt.rcParams['font.size'] = 18
+plt.xlabel('Wavenumber / $cm^-1$')
+plt.ylabel('Absorbance (A)')
+plt.plot(data1['wavenumber / cm-1'], data1_absorbance_corr, label="10 mbar (max A: 0.072)")
+plt.plot(data2['wavenumber / cm-1'], data2_absorbance_corr+0.1, label="20 mbar (max A: 0.15)")
+plt.plot(data3['wavenumber / cm-1'], data3_absorbance_corr+0.2, label="50 mbar (max A: 0.45)")
+plt.plot(data4['wavenumber / cm-1'], data4_absorbance_corr+0.3, label="100 mbar (max A: 1.25)")
+plt.plot(data5['wavenumber / cm-1'], data5_absorbance_corr+0.49, label="120 mbar (max A: 1.64)")
+
+plt.legend()
+ax = plt.gca()
+
+plt.annotate("$CO_{2}$ Vib. 1 (3800-3500 $cm^-1$)",
+            xy=(3600, 0.68), xycoords='data',
+            xytext=(3850, 0.8), textcoords='data',
+            arrowprops=dict(arrowstyle="->",
+                            connectionstyle="arc3"),
+            )
+plt.annotate("$CO_{2}$ Vib. 2 (3800-3500 $cm^-1$)",
+            xy=(2300, 1.6), xycoords='data',
+            xytext=(2200, 1.9), textcoords='data',
+            arrowprops=dict(arrowstyle="->",
+                            connectionstyle="arc3"),
+            )
+plt.annotate("$CO$ Vib. 1 (~2200-2100 $cm^-1$)",
+            xy=(2150, 0.65), xycoords='data',
+            xytext=(2200, 1.0), textcoords='data',
+            arrowprops=dict(arrowstyle="->",
+                            connectionstyle="arc3"),
+            )
+
+
+rectA = patches.Rectangle(xy=(3500,-0.05), width=300, height=0.7, linewidth=1.5, linestyle='--', edgecolor='r', facecolor='none')
+rectA_1 = patches.Rectangle(xy=(2100,-0.05), width=100, height=0.7, linewidth=1.5, linestyle='--', edgecolor='r', facecolor='none')
+ax.add_patch(rectA)
+ax.add_patch(rectA_1)
+
+plt.gca().invert_xaxis()
+plt.show()
+
+# Plot methane scans
